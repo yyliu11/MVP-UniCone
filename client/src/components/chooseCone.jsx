54f11cdcca03d36110ad350c2094d35ee3d23673
Emily Liu <emilylau211@gmail.com>
2021-08-25 00:42:58 -0700

    Implemented Welcome page and a carousel to display cone selection

--- client/src/components/chooseCone.jsx
+++ client/src/components/chooseCone.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ReactDOM from 'react-dom';
+import Carousel from 'react-elastic-carousel';
+
 
 let ChooseCone = (props) => {
-
+  const [images, setImages] = useState([
+    {id: 1, name:'Fresh-Baked Waffle Cone', url:'https://i.postimg.cc/52GWFj5c/waffle-cone.png'},
+    {id: 2, name:'Sugar Cone', url:'https://i.postimg.cc/3RtBd0pm/sugar-cone.png'},
+    {id: 3, name:'Chocolate Dipped', url:'https://i.postimg.cc/6qRfzjgL/choco-dipped-cone.png'},
+    {id: 4, name:'Kid Cone', url:'https://i.postimg.cc/SKsLHNHK/kid-cone.png'}
+  ]);
   return (
-    <div>Step 1
+    <div>
+      <label id='step'>STEP 1</label>
+      <p id='choose'>Choose Cone</p>
+      <div id='carousel'>
+      <Carousel>
+        {images.map(image => <div key={image.id}><img id='cone-img' src={image.url}></img><div>{image.name}</div></div>)}
+      </Carousel>
+      </div>
     </div>
   )
 };
--- client/src/components/homepage.jsx
+++ client/src/components/homepage.jsx
@@ -5,7 +5,7 @@ let Homepage = (props) => {
 
   return (
     <div>
-      <h1>Welcome<img id='icon' src='https://image.freepik.com/free-vector/cute-unicorn-vector-icecream-suit_70350-55.jpg'></img></h1>
+      <h1>Welcome<img id='icon' src='https://st3.depositphotos.com/1007566/34612/v/380/depositphotos_346120650-stock-illustration-cute-unicorn-ice-cream-isolated.jpg'></img></h1>
       <p>Customize your own ice cream creation!</p>
       <img id='ice-cream' src='http://images4.fanpop.com/image/photos/17800000/Yum-Yum-ice-cream-17847731-617-775.jpg'></img>
       <div><button id='btn' onClick={() => {props.toggleHomepage();props.toggleCone()}}>Tap to begin</button></div>
